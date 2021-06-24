dfc7bdb434ad81ae0431a88e72d5e59bb450f31a
subrat shrestha <z5266273@ad.unsw.edu.au>
2021-06-24 14:02:08 +1000

    generic event handler

--- new/PSOC6/AlertSystem/AlertSystem.cydsn/main_cm4.c
+++ new/PSOC6/AlertSystem/AlertSystem.cydsn/main_cm4.c
@@ -10,6 +10,43 @@
  * ========================================
 */
 #include "project.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include <stdio.h>
+#include <limits.h>
+
+#define LED_ON 0
+#define LED_OFF 1
+
+// to lock/unlock the BLE Task
+SemaphoreHandle_t bleSemaphore;
+
+
+void genericEventHandler(uint32_t event, void *eventParameter) {
+    (void) eventParameter;
+    switch(event) {
+        case CY_BLE_EVT_STACK_ON:
+            printf("Stack started\r\n");
+            Cy_BLE_GAPP_StartAdvertisement(CY_BLE_ADVERTISING_FAST, CY_BLE_PERIPHERAL_CONFIGURATION_0_INDEX);
+        break;
+            
+        case CY_BLE_EVT_GATT_CONNECT_IND:
+            printf("Connected\r\n");
+            Cy_GPIO_Write(P7_1_PORT, P7_1_PIN, LED_ON);   // GREEN LED ON: port=P7_1_PORT, pin=P7_1_PIN
+        break;
+            
+        case CY_BLE_EVT_GAP_DEVICE_DISCONNECTED:
+            printf("Disconnected\r\n");
+            Cy_GPIO_Write(P7_1_PORT, P7_1_PIN, LED_OFF);   // GREEN LED OFF
+            Cy_BLE_GAPP_StartAdvertisement(CY_BLE_ADVERTISING_FAST, CY_BLE_PERIPHERAL_CONFIGURATION_0_INDEX);
+        break;
+            
+        default:
+            break;
+    }
+}
+
 
 int main(void)
 {
